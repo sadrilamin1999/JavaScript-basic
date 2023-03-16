d1eeb3c9bcf1e63fc61c67ce344ae845a26f5a94
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-17 01:12:17 +0600

    for of and for in loop added

---
+++ for-of-in.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (myNums of numbers) {
+  console.log(myNums);
+}
+
+const even = [2, 4, 6, 8, 10, 12, 14];
+// provides index number
+for (eventNums in even) {
+  console.log(eventNums);
+}
