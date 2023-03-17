f458d9cacfb9fcd861411d8ef4bc1187e2dd5bce
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-17 15:41:39 +0600

    class added

---
+++ class.js
@@ -0,0 +1,8 @@
+class Book {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+const BookDetails = new Book("Wings of fire", "120tk");
+console.log(BookDetails);
