7e09292bf07883582ccb02e108ace2df7f5176fc
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-27 22:04:59 +0600

    post methood added

--- fetch-api/main.js
+++ fetch-api/main.js
@@ -1,4 +1,20 @@
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then((res) => res.json())
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+})
+  .then((res) => {
+    if (!res.ok) {
+      const message = `Error: ${res.status}`;
+      throw new Error(message);
+    }
+    return res.json();
+  })
   .then((res) => console.log(res))
-  .catch((error) => console.log(error));
+  .catch((err) => console.log(err));
