38a4077137730206524cb8c12e8d4ab6b4814abd
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-29 15:27:17 +0600

    local storage and get added

---
+++ local-stroge/local-stroge.js
@@ -0,0 +1,6 @@
+/* localStorage.setItem("userName", "Sadril Amin");
+localStorage.setItem("passward", "01645121"); */
+
+const user = localStorage.getItem("userName");
+const pass = localStorage.getItem("passward");
+console.log(user, pass);
