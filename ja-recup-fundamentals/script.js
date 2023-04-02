095e6b196cecace3ba189ffd63e5bd06610e5d2d
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-03 01:19:55 +0600

    fundamentals added

---
+++ ja-recup-fundamentals/script.js
@@ -0,0 +1,9 @@
+// variables
+var myName = "Sadril"; // global variable
+
+let yourName = "Amin";
+yourName = "Shuvo"; // reassaigned
+// console.log(yourName);
+
+const mentorName = "Shohanur Rahoman";
+// mentorName = "Jhankar Mahabub";// ressigned is not possible
