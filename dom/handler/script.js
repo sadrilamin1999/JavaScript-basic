ebb767793d8b0494e2ac3e7306a7b1b31e6266b7
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-03 15:46:53 +0600

    handler added

---
+++ dom/handler/script.js
@@ -0,0 +1,6 @@
+const message1 = () => {
+  document.getElementById("fristp").innerHTML = "You have clicked button 1";
+};
+const message2 = () => {
+  document.getElementById("fristp").innerHTML = "You have clicked button 2";
+};
