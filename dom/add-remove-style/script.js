51a4f87d07a3fb2a0c628986e112aa33af862d78
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-04 01:32:52 +0600

    add or remove style added

---
+++ dom/add-remove-style/script.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementsByTagName("p")[0];
+
+const add = () => {
+  paragraph.classList.add("para-style");
+};
+const remove = () => {
+  paragraph.classList.remove("para-style");
+};
