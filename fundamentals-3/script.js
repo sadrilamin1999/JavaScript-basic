dd95b8bdee80924ffb904903673fb2182d2dbc5c
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-06 01:41:16 +0600

    object distructure added

--- fundamentals-3/script.js
+++ fundamentals-3/script.js
@@ -1,4 +1,13 @@
 // distructure array
-const arr = [10, 20, 30, 40];
+/* const arr = [10, 20, 30, 40];
 const [frist, second, , fourth] = arr;
-console.log(frist, second, fourth);
+console.log(frist, second, fourth); */
+
+// distructure object
+const sutdent = {
+  fristName: "Rifat",
+  lastName: "Miha",
+  birthYear: 1999,
+};
+const { fristName, birthYear } = sutdent;
+console.log(fristName, birthYear);
