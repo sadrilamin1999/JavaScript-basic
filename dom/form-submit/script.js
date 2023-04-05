1e4e4c28755ae1f41f9d1a367df5772d190da252
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-05 15:28:57 +0600

    form sumbit added

---
+++ dom/form-submit/script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form");
+const name = form.querySelector("div #name");
+const email = form.querySelector("div #email");
+const password = form.querySelector("div #password");
+const btn = form.querySelector("div #btn");
+
+form.addEventListener("submit", submitHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+  const userInfo = {
+    name: name.value,
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userInfo);
+  name.value = "";
+  email.value = "";
+  password.value = "";
+}
