1533188a71c42507545a895e5f611e629364990a
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-09 13:06:36 +0600

    date constructor added

--- fundamentals-5/script.js
+++ fundamentals-5/script.js
@@ -19,3 +19,41 @@ console.log(Math.sqrt(4));
 console.log(Math.floor(Math.random() * 10));
 // Math.trunc
 console.log(Math.trunc(3.4));
+
+// Dare
+// new Date
+const myDate = new Date();
+console.log(myDate);
+// Date.now
+console.log(Date.now());
+// get Date
+console.log(myDate.getDate());
+// getDay
+console.log(myDate.getDay());
+// getFullYear
+console.log(myDate.getFullYear());
+// getHours
+console.log(myDate.getHours());
+//getMunites
+console.log(myDate.getMinutes());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMonth());
+
+//Time and date formator
+console.log(myDate.toLocaleDateString());
+console.log(
+  myDate.toLocaleString("bn-BD", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+);
+
+// Courency formator
+const money = 150.65;
+
+const formatedMoney = money.toLocaleString("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+console.log(formatedMoney);
