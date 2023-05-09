f4711c82aac45d7b198c86b7833e0ebb9f7e76b5
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-09 17:01:08 +0600

    BOM added

---
+++ BOM/script.js
@@ -0,0 +1,9 @@
+const locationDiv = document.querySelector(".location-div");
+
+const p1 = (locationDiv.children[0].textContent = location.href);
+
+const p2 = (locationDiv.children[1].textContent = location.hostname);
+
+const p3 = (locationDiv.children[2].textContent = location.port);
+
+const p4 = (locationDiv.children[3].textContent = location.pathname);
