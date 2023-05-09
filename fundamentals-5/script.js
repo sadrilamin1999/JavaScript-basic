1d47c3ee6d8c60670f0d3942dca45022dc464c94
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-09 12:41:51 +0600

    Math function added

---
+++ fundamentals-5/script.js
@@ -0,0 +1,21 @@
+// Math----------
+// Math.abs
+console.log(Math.abs(-2));
+// Math.ceil
+console.log(Math.ceil(4.6));
+// Math.floor
+console.log(Math.floor(4.6));
+// Math.round
+console.log(Math.round(4.8));
+// Math.max
+console.log(Math.max(15, 35));
+// Math.min
+console.log(Math.min(15, 35));
+// Math.pow
+console.log(Math.pow(2, 4));
+//Math.sqrt
+console.log(Math.sqrt(4));
+// Math.random
+console.log(Math.floor(Math.random() * 10));
+// Math.trunc
+console.log(Math.trunc(3.4));
