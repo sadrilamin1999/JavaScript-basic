8fd1049b719026fbb7f6a8f69088fd68221de78e
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-21 17:58:40 +0600

    error handler added

--- error-handling/main.js
+++ error-handling/main.js
@@ -1,8 +1,15 @@
-try {
-  console.log("Sadril");
-  console.log(middleName);
-} catch (error) {
-  console.log(error.message);
-} finally {
-  console.log("Shuvo");
-}
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  const num = document.querySelector("#textField").value;
+
+  try {
+    if (num < 10) {
+      throw "Number is low";
+    } else if (num > 20) {
+      throw "Number is to large";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
