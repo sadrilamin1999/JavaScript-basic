0cb8533a78305593eef24aa4153913c9b1674297
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-22 01:13:53 +0600

    oop1 added

---
+++ oop-1/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// constructor
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// instance
+const person1 = new Person("Sadril", 25);
+const person2 = new Person("Shohan", 28);
+/* console.log(person1.name);
+console.log(person2.name, person2.age); */
+
+// Prototypal inheritance and the Prothtype chain
