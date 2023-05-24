2daf879a903640e64ca307f2bbc2e77c788fb39f
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-05-24 17:43:49 +0600

    distructer object added

---
+++ distructerObj.js
@@ -0,0 +1,11 @@
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  middleName: "C.",
+  currentAge: 28,
+};
+let { firstName, lastName, middleName, currentAge } = person;
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(currentAge);
