6f6b113a36fb5d70ae57ae70475e45df50aad3d5
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-10-07 15:58:24 +0600

    factorial added

--- febonacci/main.js
+++ febonacci/main.js
@@ -1,6 +1,13 @@
 // fibonacchi
 let fibo = [0, 1];
-for (let i = 2; i < 8; i++) {
-  fibo[i] = fibo[i - 1] * fibo[i - 2];
+for (let i = 2; i <= 10; i++) {
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
 console.log(fibo);
+
+// factorial
+let number = 1;
+for (let i = 1; i <= 10; i++) {
+  number = number + i;
+}
+console.log(number);
