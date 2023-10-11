82a990bdce0a6c602dc9dd0e66f224a1e909923e
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-10-11 17:14:46 +0600

    inheritance added

--- febonacci/main.js
+++ febonacci/main.js
@@ -7,7 +7,7 @@ console.log(fibo);
 
 // factorial
 let number = 1;
-for (let i = 1; i <= 10; i++) {
+for (let i = 2; i <= 10; i++) {
   number = number + i;
 }
 console.log(number);
--- oop-1/script.js
+++ oop-1/script.js
@@ -1,68 +1,26 @@
 "use strict";
-
-// constructor
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-// instance
-const person1 = new Person("Sadril", 25);
-const person2 = new Person("Shohan", 28);
-/* console.log(person1.name);
-console.log(person2.name, person2.age); console.log(Person.prototype);
-console.log(person1.__proto__);
-console.log(Person.prototype === Person); */
-
-// Prototypal inheritance and the Prothtype chain
-// constructor1
-function Animal(name) {
-  this.name = name;
-}
-// prototype for constructor1
-Animal.prototype.greet = function () {
-  console.log(`Hello ${this.name}`);
-};
-
-// constructor 2
-function Cat(name, birthYear) {
-  Animal.call(this, name);
-  this.birthYear = birthYear;
-}
-
-// prototype for constructor 2
-Cat.prototype = Object.create(Animal.prototype);
-
-Cat.prototype.calAge = function () {
-  console.log(`Age: ${new Date().getFullYear() - this.birthYear} year/s`);
-};
-
-const cat1 = new Cat("Hnagry", 2018);
-/* console.log(cat1.name);
-cat1.calAge();
-cat1.greet();
- */
-
-// ES-6 Classes
-class Student {
-  constructor(name) {
+// Class
+class Player {
+  constructor(name, age, skill) {
     this.name = name;
-  }
-  greet() {
-    console.log(`Hello, ${this.name}`);
-  }
-}
-
-class FristBoy extends Student {
-  constructor(name, birthYear) {
-    super(name);
-    this.birthYear = birthYear;
-  }
-  calAge() {
-    console.log(`age: ${new Date().getFullYear() - this.birthYear}`);
+    this.age = age;
+    this.skill = skill;
   }
 }
 
-const student1 = new FristBoy("Amin", 1998);
-student1.greet();
-student1.calAge();
+const messi = new Player("Messi", 36, "Footballler");
+console.log(messi);
+const moinAli = new Player("Moin Ali", 35, "Cricketer");
+console.log(moinAli);
+
+// inheritance
+class Father extends Player {
+  constructor(name, age, skill, child) {
+    super(name, age, skill);
+    this.child = child;
+  }
+}
+
+const shuvo = new Father("Shuvo", 35, "Business man", 5);
+console.log(shuvo);
+console.log(shuvo.child);
