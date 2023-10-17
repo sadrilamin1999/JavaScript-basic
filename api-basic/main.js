1d6cc4bb5ff356dd060a3e09c52ca4c89ece3f32
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-10-17 13:59:16 +0600

    basic api done

--- api-basic/main.js
+++ api-basic/main.js
@@ -1,3 +1,4 @@
+/* const ul = document.getElementById("ul");
 const submitBtn = document.getElementById("submit-btn");
 const submitHandler = () => {
   const title = document.getElementById("title").value;
@@ -20,5 +21,42 @@ const postToSurver = (title, body, id) => {
     },
   })
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((data) => displayPost(data));
 };
+function displayPost(post) {
+  const li = document.createElement("li");
+  li.textContent = `Title: ${post.title}, Body: ${post.body}, User ID: ${post.userId}`;
+  ul.appendChild(li);
+} */
+
+const ul = document.getElementById("ul");
+const submitBtn = document.getElementById("submit-btn");
+const submitHandler = () => {
+  const title = document.getElementById("title").value;
+  const body = document.getElementById("body").value;
+  const id = document.getElementById("id").value;
+  postToServer(title, body, id);
+};
+submitBtn.addEventListener("click", submitHandler);
+
+const postToServer = (title, body, id) => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: title,
+      body: body,
+      userId: id,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => displayPost(data));
+};
+
+function displayPost(post) {
+  const li = document.createElement("li");
+  li.textContent = `Title: ${post.title}, Body: ${post.body}, User ID: ${post.userId}`;
+  ul.appendChild(li);
+}
