85ac64da7a77faaf28d901ebd58b4b8f86865c53
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-02-02 12:35:58 +0600

    hositing done

--- hoisting.js
+++ hoisting.js
@@ -2,9 +2,14 @@ x = 10;
 console.log(x);
 var x;
 
-let y = 4;
 y = 5;
-console.log(y);
-/* a=8;
-console.log(a);
-const a; */
+let y;
+console.log(y); // can't access before initialization
+
+ const a; 
+a= 5; 
+console.log(a);  // assainment to constant variable
+
+z = 6;
+let z;
+console.log(z);
